f591f0c2a5c06f6249b7c3e119027370689d41ad
Armond Sarkisian <armond.sarkisian@gmail.com>
2016-05-20 17:26:47 -0700

    slight modification to simple slots source code

--- SimpleSlots/simpleslots.py
+++ SimpleSlots/simpleslots.py
@@ -5,20 +5,23 @@ from decimal import Decimal
 
 def main( ):
 
-  slots = [[1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6, 7]]
+  slots = [["cherry", "7", "bar", "bar2", "bar3", "star", "heart"],
+          ["7", "bar", "star", "heart", "cherry", "bar2", "bar3"], 
+          ["heart", "star", "bar3", "bar", "cherry", "7", "bar2"]]
+
   money = Decimal( 1500.00 )
   playedCount = ( 0 )
  
-  while( money >= 0 ):
+  while( Decimal( money ) >= ( 0.0 ) ):
     playedCount += ( 1 )
 
     column1 = ( random.randrange( 0, len( slots[0] ), 1 ) )
     column2 = ( random.randrange( 0, len( slots[1] ), 1 ) )
     column3 = ( random.randrange( 0, len( slots[2] ), 1 ) )
 
-    print( "Played: ", str( playedCount ), " - Money: $", str( money ), "\t[", slots[0][column1], "]\t[", slots[1][column2], "]\t[", slots[2][column3], "]", sep = ( "" ) )
+    print( "Played: ", str( playedCount ), " - Money: $", str( money ), "\t[", slots[0][column1], "][", slots[1][column2], "][", slots[2][column3], "]", sep = ( "" ) )
     
-    if( column1 == column2 == column3 ):
+    if( slots[0][column1] == slots[1][column2] == slots[2][column3] ):
       money += ( Decimal( 100.00 ) )
       print( "You win!" )
     else:
