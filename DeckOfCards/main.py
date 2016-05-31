6776554eb4775897e8737293d0adbffdd944b612
Armond Sarkisian <armond.sarkisian@gmail.com>
2016-05-31 08:32:16 -0700

    more source code

---
+++ DeckOfCards/main.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+def shuffle( li ):
+  tmpList = []
+
+  # traversing through all the cards
+  for x in range( 0, li, 1 ):
+    
+
+
+def display( li ):
+  for i in li
+    print( i )
+
+
+def main( ):
+  deck = [ 
+          "AH", 
+          "2H", 
+          "3H", 
+          "4H", 
+          "5H", 
+          "6H", 
+          "7H", 
+          "8H", 
+          "9H", 
+          "10H", 
+          "JH", 
+          "QH", 
+          "KH",
+          "AC", 
+          "2C", 
+          "3C", 
+          "4C", 
+          "5C", 
+          "6C", 
+          "7C", 
+          "8C", 
+          "9C", 
+          "10C", 
+          "JC", 
+          "QC", 
+          "KC",
+          "AD", 
+          "2D", 
+          "3D", 
+          "4D", 
+          "5D", 
+          "6D", 
+          "7D", 
+          "8D", 
+          "9D", 
+          "10D", 
+          "JD", 
+          "QD", 
+          "KD",
+          "AS", 
+          "2S", 
+          "3S", 
+          "4S", 
+          "5S", 
+          "6S", 
+          "7S", 
+          "8S", 
+          "9S", 
+          "10S", 
+          "JS", 
+          "QS", 
+          "KS"
+         ]
+
+  display( deck )
+  shuffle( deck )
+  display ( deck )
+
+if( __name__ == ( "__main__" ) ):
+  main( )
---
+++ HelloWorld2/main.py
@@ -0,0 +1,8 @@
+def display( txt ):
+  print( txt )
+
+def main( ):
+  display( "Hello, World!" )
+
+if( __name__ == ( "__main__" ) ):
+  main( )
