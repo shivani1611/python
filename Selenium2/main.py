7edd86e314d09148737eb059b8464767347f9062
Armond Sarkisian <armond.sarkisian@gmail.com>
2016-06-22 19:05:27 -0700

    just fiddling around with selenium

---
+++ Selenium2/main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox( )
+
+for i in range( 0, 100, 1 ):
+  driver.get( "http://www.python.org" )
+  driver.get( "http://www.google.com" )
+  driver.get( "http://www.ebay.com" )
+  driver.get( "http://www.oracle.com" )
