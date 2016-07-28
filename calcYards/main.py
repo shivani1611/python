cafdaa2003580660752ff5b943228e4e05e58d5e
Armond Sarkisian <armond.sarkisian@gmail.com>
2016-07-27 21:52:23 -0700

    changed to CalcYards

--- calcYards/main.py
+++ calcYards/main.py
@@ -2,9 +2,11 @@
 
 import sys
 
+################################
 def calcYards( feet ):
   return( feet / 3 )
 
+################################
 def main( ):
   feet = ( input( "Enter feet to calculate yards: " ) )
   print( "{0:.2f} converted into yards is {1:.2f}".format( round( float( feet ), 2 ), round( calcYards( float( feet ) ), 2 ) ) )
