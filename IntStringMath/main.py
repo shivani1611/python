7340b2f450562f1cf30cee0208649cb6c7875dda
Armond Sarkisian <asarkisian@yp.com>
2016-09-26 15:50:39 -0700

    Added IntStringMath 500+60+7

---
+++ IntStringMath/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+
+def convToString( num ):
+  num_length = len( str( num ) )
+  count = num_length
+
+  conv_string = ""
+
+  for i in range( 0, num_length, 1 ):
+    conv_string += ( str( num )[i] )
+
+    if( count > 1 ):
+      conv_string += ( str( "0" * ( count - 1 ) ) )
+      conv_string += ( "+" )
+
+    count = ( count - 1 )
+
+  return( conv_string )
+
+def main( ):
+  print( convToString( 567 ) )
+
+if( __name__ == ( "__main__" ) ):
+  sys.exit( main( ) )
