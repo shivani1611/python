5c6d39a181d2079e20473a765e052c9100fb790e
asarkisian <armond.sarkisian@gmail.com>
2016-11-30 15:58:49 -0800

    added apiproject_testing 01

---
+++ APIProject_01/tools/mysql_connect.py
@@ -0,0 +1,68 @@
+from pymysql import connect
+
+class Mysql_Connect( object ):
+
+    def __init__( self ):
+        pass
+
+    def __connect( self, host, port, user, db  ):
+
+        # establish the connection
+        conn = ( connect( host = ( host ), 
+                          port = ( port ), 
+                          user = ( user ), 
+                          db = ( db ) ), )
+
+        return conn
+
+    def select( self, db, query ):
+
+        # make a call to establish the connection
+        conn = ( self.__connect( db ) )
+
+        # establish the cursor
+        cur  = ( self._conn.cursor( ) )
+
+        # execute the specified query
+        cur.execute( query )
+
+        # retrieve results from query
+        result = ( cur.fetchall( ) )
+
+        # organize the results
+        all_rows = []
+        for line in result:
+            row = []
+            for col in line:
+                row.append( str( col ) )
+            all_rows.append( row )
+
+        # close the connection
+        conn.close( )
+
+        # close the cursor
+        cur.close( ) 
+
+        return all_rows
+
+def update( self, db, query ):
+
+    # make a call to establish the connection
+    conn = self.__connect( db )
+
+    # establish the cursor
+    cur  = conn.cursor( )
+
+    # retrieve results from query
+    result = ( cur.execute( query ) )
+
+    # save the results
+    conn.commit( )
+
+    # close the connection
+    conn.close( )
+
+    # close the cursor
+    cur.close( )
+
+    return result
---
+++ APIProject_01/tools/request.py
@@ -0,0 +1,39 @@
+from woocommerce import API
+
+class Request( object ):
+
+    def __init__( self ):
+        url             = ( r"http://127.0.0.1/api_test01/" )
+        consumer_key    = ( r"ck_3c39facaa33d0e7dd953167c6f627a4d25022f18" )
+        consumer_secret = ( r"cs_967e8d9ecd7e75f2254d5a15aaae3a13339235e7" )
+        wp_api          = ( True )
+        version         = ( r"wc/v1" )
+
+        self._wcapi = API(
+            url             = ( url ),
+            consumer_key    = ( consumer_key ),
+            consumer_secret = ( consumer_secret ),
+            wp_api          = ( wp_api ),
+            version         = ( version ), )
+
+        return None
+
+    def test_api_connection( self ):
+        return self._wcapi.get( "" ).json( )
+
+    def post(self, end_point, pay_load ):
+        result        = ( self._wcapi.post( end_point, pay_load ) )
+        response_code = ( result.status_code )
+        body          = ( result.json( ) )
+        url           = ( result.url )
+
+        return [response_code, body, url] 
+
+    def get( self, end_point ):
+        result        = ( self._wcapi.get( end_point ) )
+        response_code = ( result.status_code )
+        body          = ( result.json( ) )
+        url           = ( result.url )
+
+        return [response_code, body, url] 
+
