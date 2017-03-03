cac565acdcfd8eace2c23ca88ce9c1eb4eb212dd
Armond Sarkisian <armond.sarkisian@gmail.com>
2017-03-02 21:23:37 -0800

    updated

--- TexasHoldemPoker/main.py
+++ TexasHoldemPoker/main.py
@@ -5,14 +5,207 @@ from sys import exit
 
 class TexasHoldemPoker(object):
     
-    my_deck = []
+    # entire deck
+    the_deck = []
 
-    @staticmethod
-    def generate_random_number( ):
+    # hands
+    human_hand = []
+    computer_hand = []
+    table_hand = []
 
-        rand_num = randint( 1, 52 )
+    # money
+    PAY_PER_HAND = 5.00
+    the_balance = 500.00
 
-        return rand_num
+    @classmethod
+    def display_deck( cls ):
+        for i in cls.the_deck:
+            print( "All Cards: ", i )
+
+
+    @classmethod
+    def display_human_hand( cls ):
+        print( "Human Hand:\t", end = ' ' )
+        for i in cls.human_hand:
+            print( i , end = ' ')
+        print( )
+
+
+    @classmethod
+    def display_computer_hand( cls ):
+        print( "Computer Hand:\t", end = ' ' )
+        for i in cls.computer_hand:
+            print( i , end = ' ')
+        print( )
+
+
+    @classmethod
+    def display_table_hand( cls ):
+        print( "Table Hand:\t", end = ' ' )
+        for i in cls.table_hand:
+            print( i , end = ' ')
+        print( )
+
+
+    def _prep_hands( cls, who ):
+
+        temp_deck = None
+
+        who = str( who ).strip( ).lower( )
+
+        if who == "human":
+            cls.human_hand.extend( cls.table_hand )
+            temp_deck = cls.human_hand
+        elif who == "computer":
+            cls.computer_hand.extend( cls.table_hand )
+            temp_deck = cls.computer_hand
+        else:
+            raise ValueError( "Error: Not able to determine whose hand to prep" )
+
+        return temp_deck
+
+
+    @classmethod
+    def determine_high_card( cls, who )
+
+        isAce = False
+        isKing = False
+        isQueen = False
+        isJack = False
+        isTen = False
+        isNine = False
+        isEight = False
+        isSeven = False
+        isSix = False
+        isFive = False
+        isFour = False
+        isThree = False
+        isTwo = False
+
+        temp_high_card = None
+
+        # determine high card for specified player
+        temp_deck = _prep_hands( who )
+
+        # determine the highest card for the hand
+        for i in temp_deck:
+            if i[0] == 'A':
+                isAce = True
+            elif i[0] == 'K':
+                isKing = True
+            elif i[0] == 'Q':
+                isQueen = True
+            elif i[0] == 'J':
+                isJack = True
+            elif i[0] == '1':
+                isTen = True
+            elif i[0] == '9':
+                isNine = True
+            elif i[0] == '8':
+                isEight = True
+            elif i[0] == '7':
+                isSeven = True
+            elif i[0] == '6':
+                isSix = True
+            elif i[0] == '5':
+                isFive = True
+            elif i[0] == '4':
+                isFour = True
+            elif i[0] == '3':
+                isThree = True
+            elif i[0] == '2':
+                isTwo = True
+            else:
+                raise ValueError( "Error: Not able to determine highest card" )
+        
+        if isAce:
+            temp_high_card = 14
+        elif isKing:
+            temp_high_card = 13
+        elif isQueen:
+            temp_high_card = 12
+        elif isJack:
+            temp_high_card = 11
+        elif isTen:
+            temp_high_card = 10
+        elif isNine:
+            temp_high_card = 9
+        elif isEight:
+            temp_high_card = 8
+        elif isSeven:
+            temp_high_card = 7
+        elif isSix:
+            temp_high_card = 6
+        elif isFive:
+            temp_high_card = 5
+        elif isFour:
+            temp_high_card = 4
+        elif isThree:
+            temp_high_card = 3
+        elif isTwo:
+            temp_high_card = 2
+        else:
+            raise ValueError( "Error: Not able to determine highest card" )
+
+        return temp_high_card
+
+
+    @classmethod
+    def determine_hand( cls, who ):
+
+        hand_value = None
+
+        # determine whose hand to evaluate
+        temp_deck = _prep_hands( who )
+
+        # determine if royal flush
+        hand_value = 10
+
+        # determine if straight flush
+        hand_value = 9
+
+        # determine if four of a kind
+        hand_value = 8
+
+        # determine if full house
+        hand_value = 7
+
+        # determine flush
+        hand_value = 6
+
+        # determine straight
+        hand_value = 5
+
+        # determine if three of a kind
+        hand_value = 4
+
+        # determine if two pair
+        hand_value = 3
+
+        # determine if pair
+        hand_value = 2
+
+        # determine highest card
+        hand_value = 1
+
+        return hand_value
+
+
+    @classmethod
+    def deal_card( cls, who ):
+
+        who = str( who ).strip( ).lower( )
+
+        if who == "human":
+            cls.human_hand.append( cls.the_deck.pop( ) )
+        elif who == "computer":
+            cls.computer_hand.append( cls.the_deck.pop( ) )
+        elif who == "table":
+            cls.table_hand.append( cls.the_deck.pop( ) )
+        elif who == "trash":
+            cls.the_deck.pop( )
+        else:
+            raise ValueError( "Error: Not able to determine who to deal cards to!" )
 
 
     @classmethod
@@ -25,104 +218,97 @@ class TexasHoldemPoker(object):
             rand_num = randint( 0, 51 )
 
             if rand_num not in elements_used:
-                print (rand_num )
                 elements_used.append( rand_num )
-                temp_deck.append( cls.my_deck[rand_num] )
+                temp_deck.append( cls.the_deck[rand_num] )
 
-                if len( temp_deck ) == len( cls.my_deck ):
+                if len( temp_deck ) == len( cls.the_deck ):
                     break
 
-        cls.my_deck = temp_deck
-
-
-    @classmethod
-    def display_deck( cls ):
-        for i in cls.my_deck:
-            print( "Card: ", i )
+        cls.the_deck = temp_deck
 
 
     @classmethod
     def initialize_deck( cls ):
 
-        my_deck = []
+        the_deck = []
 
         # Ace
-        cls.my_deck.append( "AC" )
-        cls.my_deck.append( "AH" )
-        cls.my_deck.append( "AS" )
-        cls.my_deck.append( "AD" )
+        cls.the_deck.append( "AC" )
+        cls.the_deck.append( "AH" )
+        cls.the_deck.append( "AS" )
+        cls.the_deck.append( "AD" )
 
         # Two
-        cls.my_deck.append( "2C" )
-        cls.my_deck.append( "2H" )
-        cls.my_deck.append( "2S" )
-        cls.my_deck.append( "2D" )
+        cls.the_deck.append( "2C" )
+        cls.the_deck.append( "2H" )
+        cls.the_deck.append( "2S" )
+        cls.the_deck.append( "2D" )
 
         # Three
-        cls.my_deck.append( "3C" )
-        cls.my_deck.append( "3H" )
-        cls.my_deck.append( "3S" )
-        cls.my_deck.append( "3D" )
+        cls.the_deck.append( "3C" )
+        cls.the_deck.append( "3H" )
+        cls.the_deck.append( "3S" )
+        cls.the_deck.append( "3D" )
 
         # Four
-        cls.my_deck.append( "4C" )
-        cls.my_deck.append( "4H" )
-        cls.my_deck.append( "4S" )
-        cls.my_deck.append( "4D" )
+        cls.the_deck.append( "4C" )
+        cls.the_deck.append( "4H" )
+        cls.the_deck.append( "4S" )
+        cls.the_deck.append( "4D" )
 
         # Five
-        cls.my_deck.append( "5C" )
-        cls.my_deck.append( "5H" )
-        cls.my_deck.append( "5S" )
-        cls.my_deck.append( "5D" )
+        cls.the_deck.append( "5C" )
+        cls.the_deck.append( "5H" )
+        cls.the_deck.append( "5S" )
+        cls.the_deck.append( "5D" )
 
         # Six
-        cls.my_deck.append( "6C" )
-        cls.my_deck.append( "6H" )
-        cls.my_deck.append( "6S" )
-        cls.my_deck.append( "6D" )
+        cls.the_deck.append( "6C" )
+        cls.the_deck.append( "6H" )
+        cls.the_deck.append( "6S" )
+        cls.the_deck.append( "6D" )
 
         # Seven
-        cls.my_deck.append( "7C" )
-        cls.my_deck.append( "7H" )
-        cls.my_deck.append( "7S" )
-        cls.my_deck.append( "7D" )
+        cls.the_deck.append( "7C" )
+        cls.the_deck.append( "7H" )
+        cls.the_deck.append( "7S" )
+        cls.the_deck.append( "7D" )
 
         # Eight
-        cls.my_deck.append( "8C" )
-        cls.my_deck.append( "8H" )
-        cls.my_deck.append( "8S" )
-        cls.my_deck.append( "8D" )
+        cls.the_deck.append( "8C" )
+        cls.the_deck.append( "8H" )
+        cls.the_deck.append( "8S" )
+        cls.the_deck.append( "8D" )
 
         # Nine
-        cls.my_deck.append( "9C" )
-        cls.my_deck.append( "9H" )
-        cls.my_deck.append( "9S" )
-        cls.my_deck.append( "9D" )
+        cls.the_deck.append( "9C" )
+        cls.the_deck.append( "9H" )
+        cls.the_deck.append( "9S" )
+        cls.the_deck.append( "9D" )
 
         # Ten
-        cls.my_deck.append( "10C" )
-        cls.my_deck.append( "10H" )
-        cls.my_deck.append( "10S" )
-        cls.my_deck.append( "10D" )
+        cls.the_deck.append( "10C" )
+        cls.the_deck.append( "10H" )
+        cls.the_deck.append( "10S" )
+        cls.the_deck.append( "10D" )
 
         # Jack
-        cls.my_deck.append( "JC" )
-        cls.my_deck.append( "JH" )
-        cls.my_deck.append( "JS" )
-        cls.my_deck.append( "JD" )
+        cls.the_deck.append( "JC" )
+        cls.the_deck.append( "JH" )
+        cls.the_deck.append( "JS" )
+        cls.the_deck.append( "JD" )
 
         # Queen
-        cls.my_deck.append( "QC" )
-        cls.my_deck.append( "QH" )
-        cls.my_deck.append( "QS" )
-        cls.my_deck.append( "QD" )
+        cls.the_deck.append( "QC" )
+        cls.the_deck.append( "QH" )
+        cls.the_deck.append( "QS" )
+        cls.the_deck.append( "QD" )
 
         # King
-        cls.my_deck.append( "KC" )
-        cls.my_deck.append( "KH" )
-        cls.my_deck.append( "KS" )
-        cls.my_deck.append( "KD" )
+        cls.the_deck.append( "KC" )
+        cls.the_deck.append( "KH" )
+        cls.the_deck.append( "KS" )
+        cls.the_deck.append( "KD" )
 
 def main( ):
 
@@ -130,7 +316,27 @@ def main( ):
 
     thp.initialize_deck( )
     thp.shuffle_deck( )
-    thp.display_deck( )
+
+    thp.deal_card( "computer" )
+    thp.deal_card( "human" )
+
+    thp.deal_card( "computer" )
+    thp.deal_card( "human" )
+
+    thp.deal_card( "table" )
+    thp.deal_card( "table" )
+    thp.deal_card( "table" )
+    thp.deal_card( "table" )
+    thp.deal_card( "table" )
+
+    # check this to determine the players hand
+    human_hand_value = thp.determine_hand( "human" )
+    computer_hand_value = thp.determine_hand( "computer" )
+
+    # only check this if the hand value is equal
+    human_highest_card = thp.determine_high_card( "human" )
+    computer_highest_card = thp.determine_high_card( "computer" )
+    
 
 if __name__ == "__main__":
     exit( main( ) )
